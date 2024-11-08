5a73fac754588ff43142f83d435de80d90da3b31
Valerio Battaglia <163680081+navaa2a@users.noreply.github.com>
2024-11-08 09:21:48 +0100

    Update scriptCalc2.js

--- scriptCalc2.js
+++ scriptCalc2.js
@@ -8,32 +8,12 @@ function cancella() {
     document.getElementById("schermo").value = "";
 }
 
-
 function risultato() {
     const schermo = document.getElementById("schermo");
-    const espressione = schermo.value;
-
-
-    const risultato = eval(espressione);
-    
+        schermo.value = eval(schermo.value);
+}
 
 
 }
 
 
-document.addEventListener("keydown", function(event) {
-    const schermo = document.getElementById("schermo");
-    const simboliAmmessi = "0123456789+-*";
-
-    if (event.key === "Enter") {
-        risultato();
-    }
-    
-    else if (event.key === "Backspace") {
-        cancella();
-    }
-    
-    else if (simboliAmmessi.includes(event.key)) {
-        schermo.value += event.key;
-    }
-});
