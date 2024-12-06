91a03e97431ba78a8ad7c578464328bc4ed185c4
Valerio Battaglia <163680081+navaa2a@users.noreply.github.com>
2024-12-06 09:19:33 +0100

    Update scriptCalc2.js

--- scriptCalc2.js
+++ scriptCalc2.js
@@ -8,8 +8,30 @@ function cancella() {
 }
 
 
+
 function risultato() {
     const schermo = document.getElementById("schermo");
-        schermo.value = eval(schermo.value);
+    const espressione = schermo.value;
+    const regex = /(\d+)([+\-*/])(\d+)/g;
+    let risultato = espressione;
+
+    while (regex.test(risultato)) {
+        risultato = risultato.replace(regex, function(_, num1, operatore, num2) {
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            switch (operatore) {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    return num1 / num2;
+            }
+        });
+    }
+
+    schermo.value = risultato;
 }
 
