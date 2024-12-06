b06c2f77869fae204faa5a8aaf4be4ffba17f4ef
Valerio Battaglia <163680081+navaa2a@users.noreply.github.com>
2024-12-06 09:28:01 +0100

    Update scriptCalc2.js

--- scriptCalc2.js
+++ scriptCalc2.js
@@ -11,27 +11,60 @@ function cancella() {
 
 function risultato() {
     const schermo = document.getElementById("schermo");
-    const espressione = schermo.value;
-    const regex = /(\d+)([+\-*/])(\d+)/g;
-    let risultato = espressione;
+    let espressione = schermo.value;
 
-    while (regex.test(risultato)) {
-        risultato = risultato.replace(regex, function(_, num1, operatore, num2) {
-            num1 = parseFloat(num1);
-            num2 = parseFloat(num2);
-            switch (operatore) {
-                case '+':
-                    return num1 + num2;
-                case '-':
-                    return num1 - num2;
-                case '*':
-                    return num1 * num2;
-                case '/':
-                    return num1 / num2;
-            }
-        });
-    }
+    espressione = calcola(espressione, ['*', '/']);
+    espressione = calcola(espressione, ['+', '-']);
 
-    schermo.value = risultato;
+    schermo.value = espressione;
 }
 
+function calcola(espressione, operatori) {
+    let risultato = "";
+    let numeroParziale = "";
+    let operatorePrecedente = null;
+    let numeroPrecedente = null;
+
+    for (let i = 0; i < espressione.length; i++) {
+        let carattere = espressione[i];
+
+        if (carattere >= '0' && carattere <= '9') {
+            numeroParziale += carattere;
+        } else if (operatori.includes(carattere)) {
+            if (numeroParziale !== "") {
+                if (operatorePrecedente === null) {
+                    numeroPrecedente = parseFloat(numeroParziale);
+                } else {
+                    numeroPrecedente = eseguiOperazione(numeroPrecedente, parseFloat(numeroParziale), operatorePrecedente);
+                }
+                numeroParziale = "";
+            }
+            operatorePrecedente = carattere;
+        }
+    }
+
+    if (numeroParziale !== "") {
+        if (operatorePrecedente === null) {
+            risultato = parseFloat(numeroParziale);
+        } else {
+            risultato = eseguiOperazione(numeroPrecedente, parseFloat(numeroParziale), operatorePrecedente);
+        }
+    }
+
+    return risultato.toString();
+}
+
+function eseguiOperazione(num1, num2, operatore) {
+    switch (operatore) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        default:
+            return num1;
+    }
+}
